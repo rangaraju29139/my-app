d619049019af5aa3dad105fe88b033f111f81adf
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-15 03:06:27 +0530

    added and imported all the sub components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
 import "./App.css";
-import Container from "react-bootstrap/Container";
+import { Container, Row, Col } from "react-bootstrap";
 import Navigation from "./components/Navigation";
+import Description from "./components/Description";
+import Journey from "./components/Journey";
+import CodeProfile from "./components/CodeProfile";
+import Skills from "./components/Skills";
+import Projects from "./components/Footer";
+import Connect from "./components/Connect";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Container className="app-container">
-        <Navigation>This is Navigation</Navigation>
+        <Row ClassName="navigation">
+          <Navigation />
+        </Row>
+        <Row ClassName="navigation">
+          <Navigation />
+        </Row>
+        <Row ClassName="description">
+          <Description />
+        </Row>
+        <Row ClassName="journey">
+          <Journey />
+        </Row>
+        <Row ClassName="code-profile">
+          <CodeProfile />
+        </Row>
+        <Row ClassName="skills">
+          <Skills />
+        </Row>
+        <Row ClassName="projects">
+          <Projects />
+        </Row>
+        <Row ClassName="connect">
+          <Connect />
+        </Row>
+        <Row ClassName="Footer">
+          <Footer />
+        </Row>
       </Container>
       {/* <Navigation /> */}
     </>
