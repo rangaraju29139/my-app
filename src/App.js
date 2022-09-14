af95695666ea680c4fa1cdf935536ee2bda6d038
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-15 02:46:22 +0530

    added Navigation Component with sample content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
-      <div>Portfolio website for rangaraju penmetsa</div>
+      <Container className="app-container">
+        <Navigation>This is Navigation</Navigation>
+      </Container>
+      {/* <Navigation /> */}
     </>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Navigation(props) {
+  return <div>This is navigation </div>;
+}
+export default Navigation;
