bf47a7af61d0bebfc921a8b6b74b92289cd80c97
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-15 15:47:40 +0530

    added the btech in journey component

--- src/components/Journey.js
+++ src/components/Journey.js
@@ -5,6 +5,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import Btech from "./journey-timeline/Btech";
 import Scaler from "./journey-timeline/Scaler";
 import Tcs from "./journey-timeline/Tcs";
 
@@ -21,18 +22,7 @@ function Journey() {
           <VerticalTimeline>
             <Scaler />
             <Tcs />
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2008 - 2010"
-              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-              icon={""}
-            >
-              <h3 className="vertical-timeline-element-title">Web Designer</h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Los Angeles, CA
-              </h4>
-              <p>User Experience, Visual Design</p>
-            </VerticalTimelineElement>
+            <Btech />
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
               date="2006 - 2008"
