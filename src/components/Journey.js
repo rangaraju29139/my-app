1aef85a9b4dd8cb12732c478fbb2f8d099854438
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-15 16:09:48 +0530

    removed not used package from import

--- src/components/Journey.js
+++ src/components/Journey.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Btech from "./journey-timeline/Btech";
 import Intermediate from "./journey-timeline/Intermediate";
@@ -29,24 +26,6 @@ function Journey() {
             <Intermediate />
             <Ssc />
             <Birth />
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="2002 - 2006"
-              iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-              icon={""}
-            >
-              <h3 className="vertical-timeline-element-title">
-                Bachelor of Science in Interactive Digital Media Visual Imaging
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Bachelor Degree
-              </h4>
-              <p>Creative Direction, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
-              icon={""}
-            />
           </VerticalTimeline>
         </Row>
       </Container>
