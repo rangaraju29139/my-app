159f18833b3d62f5c91f98ee9e9d21d45970f64b
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-15 16:10:18 +0530

    removed not used packages from import

--- src/components/journey-timeline/Scaler.js
+++ src/components/journey-timeline/Scaler.js
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 function Scaler() {
   return (
