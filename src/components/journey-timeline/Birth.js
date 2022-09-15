9164837f0be9a17e535a78a152a86e08ac696906
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-15 15:59:47 +0530

    created birth as sperate compnent

---
+++ src/components/journey-timeline/Birth.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+function Birth() {
+  return (
+    <>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="july 2019 - March 2022"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">August 12 1997.</h3>
+        {/* <h4 className="vertical-timeline-element-subtitle">
+          Bhimavaram, Andhra Pradesh,India
+        </h4>
+        <p></p> */}
+      </VerticalTimelineElement>
+    </>
+  );
+}
+
+export default Birth;
