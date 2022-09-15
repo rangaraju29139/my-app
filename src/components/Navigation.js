3112e028e496f29a5848cd0b0dd3a8c7ef73c9f3
rangaraju29139 <rangaraju29139@gmail.com>
2022-09-15 14:29:12 +0530

    added links to navigations

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,26 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Navigation() {
-  return <Container>This is the Navigation component</Container>;
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" fixed="top" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">rangaraju.in</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="justify-content-end ms-auto">
+            {/* <Nav.Link href="#Description">Short Description</Nav.Link> */}
+            <Nav.Link href="#journey">My Journey</Nav.Link>
+            <Nav.Link href="#code-profile">Code Profile</Nav.Link>
+            <Nav.Link href="#skill">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#connect">Connect with me </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 export default Navigation;
